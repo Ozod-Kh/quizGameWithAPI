05a5674137f61c2b240aaf54b049db4c22c9256b
Ozod Khonberdiyev <khonberdiyev@gmail.com>
2020-10-16 03:36:28 +0300

    fixed the bug in answer comparing

--- game.js
+++ game.js
@@ -11,17 +11,22 @@ let progressBarMeasure = document.getElementById('progressBarMeasure')
 
 let questions = [];
 fetch("https://opentdb.com/api.php?amount=15&category=9&difficulty=easy&type=multiple").then(res =>{
-    console.log(res);
+    
     return res.json()
 }).then(loadedQuestions =>{
-    console.log(loadedQuestions);
+    
     questions = loadedQuestions.results.map(loadedQuestion =>{
     let questionList = {
         question: loadedQuestion.question
      }
-     questionList.answerIndex = Math.floor(Math.random *3) +1
+     
+     
+     questionList.answerIndex = Math.floor(Math.random() *3) +1
+    //  console.log(questionList.answerIndex);
      let answerChoices = [...loadedQuestion.incorrect_answers]
+      
      let theAnswer = loadedQuestion.correct_answer
+    //  console.log(theAnswer);
      answerChoices.splice(questionList.answerIndex-1, 0, theAnswer)
      answerChoices.forEach( (choice, index) =>{
          questionList["choice" + (index +1)] =choice
@@ -36,7 +41,7 @@ fetch("https://opentdb.com/api.php?amount=15&category=9&difficulty=easy&type=mul
 
 
 let answerPoints = 5
-let max_questions = 15
+let max_questions = 10
 
 function  startGame() {
     score = 0;
@@ -81,17 +86,20 @@ choices.forEach(choice =>{
         acceptAnswers = false
 
         const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        const selectedAnswer = parseInt(selectedChoice.dataset['number'])
+        console.log( selectedAnswer);
+        console.log(currentQuestion.answerIndex);
         
     
-        let answerClass= selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+        let answerClass= selectedAnswer == currentQuestion.answerIndex ? 'correct' : 'incorrect'
          selectedChoice.parentElement.classList.add(answerClass)
 
          setTimeout(() => {
             selectedChoice.parentElement.classList.remove(answerClass)
             
-            if (selectedAnswer == currentQuestion.answer) {
+            if (selectedAnswer == currentQuestion.answerIndex) {
                 incrementScore(answerPoints)
+                
             }
             getQuestion()
             
